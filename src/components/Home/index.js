803d4c0cf5e530d8afa52897b290c0771d763cd3
dev-soks <victoryasokomeh@josla.com.ng>
2020-06-25 13:46:43 +0100

    handover

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,25 +1,38 @@
 import React, { Fragment } from "react";
-import { ReactComponent as Logo } from "assets/logo.svg";
+import { ReactComponent as Logo2 } from "assets/logo2.svg";
+import { ReactComponent as Mail } from "assets/mail.svg";
 import { ReactComponent as Phone } from "assets/phone.svg";
 import { ReactComponent as Location } from "assets/location.svg";
+import bags from "assets/bags.svg";
+import butter from "assets/plate.svg";
+import logo from "assets/logo.png";
+import grid1 from "assets/grid_img.svg";
+import Button from "components/Button";
 import ContactForm from "../ContactForm";
-import { hero, container, main, contactform, footer, items, hero_text } from "./style.module.css";
+import { main, contact, contactform, footer, items, hero_text, grid } from "./style.module.css";
 
 const Home = () => {
   return (
     <Fragment>
-      <header className={container}>
-        <nav className="text-center">
-          <Logo />
+      <header>
+        <nav>
+          <Logo2 />
+          <div className={contact}>
+            <div>
+              <Phone />
+              <p>08130302400</p>
+            </div>
+            <div>
+              <Mail />
+              <p>shea-sales@ladgroup.org</p>
+            </div>
+          </div>
         </nav>
-      </header>
-      <section className={hero}>
-        {/* <div className="text-center pt-5"> */}
         <div className={hero_text}>
-          <h1 className="text-light">Pioneering in Seed-Oil production in Nigeria</h1>
+          <h1 className="text-light">Pioneering in oil-seed processing in Nigeria</h1>
           <p className="text-light">
-            Welcome, Kindly note that our website is currently under development
-            </p>
+            Welcome, kindly note that our website is currently under development
+          </p>
         </div>
         {/* <div className="duration text-light">
             <span>
@@ -29,15 +42,15 @@ const Home = () => {
             <span>
               <sup>HOURS</sup>
               <h1>10</h1>
-            </span>
+              </span>
             <span>
-              <sup>MINUTES</sup>
-              <h1>10</h1>
+            <sup>MINUTES</sup>
+            <h1>10</h1>
             </span>
           </div> */}
         {/* </div> */}
-      </section>
-      <section className={main}>
+      </header>
+      {/* <section className={main}>
         <div className="container py-4">
           <div className="text-center">
             <h4>Are you a dealer? If yes, please fill the form below</h4>
@@ -47,14 +60,52 @@ const Home = () => {
             <ContactForm />
           </div>
         </div>
-      </section>
+      </section> */}
+
+      <main>
+        <h1>LadShea</h1>
+        <p>100% natural, organic, refined shea butter with no smoky smell</p>
+
+        <section className={grid}>
+          <img src={butter} alt="bowl of butter" />
+          <aside>
+            <h2>General Enquiry</h2>
+            <p>
+              Want to make an enquiry about our different range of products and services? Feel free
+              to reach out to us.
+            </p>
+            <Button label="Make enquiry" />
+          </aside>
+        </section>
+
+        <section className={grid}>
+          <aside>
+            <h2>General Enquiry</h2>
+            <p>
+              Are you a distributor in the shea butter sector? Kindly click on the link below to
+              register as one of our distributors.
+            </p>
+            <Button label="Register" />
+          </aside>
+          <img src={bags} alt="agro sacks" />
+        </section>
+
+        <section className={grid}>
+          <img src={grid1} alt="nuts" />
+          <aside>
+            <h2>General Enquiry</h2>
+            <p>
+              Do you deal in supplying of resources and equipments (e.g sheanut, diesel, spare
+              parts, etc) Why not join our list of suppliers? .
+            </p>
+            <Button label="Register" />
+          </aside>
+        </section>
+      </main>
       <footer className={`text-center py-5 ${footer}`}>
         <h2>LadGroup</h2>
+        <img src={logo} alt="ladgroup logo" />
         <div className={`${items}`}>
-          <div>
-            <Location />
-            <p>Office address - 24, Abimbola street, Ilasamaja Isolo, Lagos, Nigeria</p>
-          </div>
           <div>
             <Location />
             <p>
@@ -63,8 +114,8 @@ const Home = () => {
             </p>
           </div>
           <div>
-            <Phone />
-            <p>08130302400, shea-sales@ladgroup.org</p>
+            <Location />
+            <p>Office address - 24, Abimbola street, Ilasamaja Isolo, Lagos, Nigeria</p>
           </div>
         </div>
       </footer>
