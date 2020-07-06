37660e58045775a7050fa8a65236f3e8bcc3a2e9
dev-soks <victoryasokomeh@josla.com.ng>
2020-07-06 02:32:01 +0100

    implementing footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {ReactComponent as Logo } from "assets/logo2.svg"
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img src={logo} alt=""/>
+        
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
